579347f63b458166641f88c8527a7f127d66fa98
woander <woander@users.noreply.github.com>
2016-02-15 13:54:05 -0500

    first commit for thenewboston bucky tutorial

---
+++ thenewbostonbuckytutorial.py
@@ -0,0 +1,28 @@
+#printing
+print('hello world')
+
+#variable name with data attached
+the_num_two = 2
+the_num_three = 3
+#printing addition problem using variables
+print(the_num_two + the_num_three)
+
+#subtraction
+print(5-3)
+#multiplying
+print(2 * 3)
+
+#dividing
+print(10/2)
+
+#dividing with rounding down
+print(10//2)
+
+#modulo
+print(11%2)
+
+#exponentiation
+print(10**2)
+
+#order of operations
+print((10+2)*5)
