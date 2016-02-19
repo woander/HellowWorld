23d0073393ecb84f334ebc42aa4825c38e3a924d
woander <woander@users.noreply.github.com>
2016-02-19 11:18:12 -0500

    I'm kind of lost

---
+++ firstwebcrawler.py
@@ -0,0 +1,19 @@
+# A Web crawler is an Internet bot which systematically browses the World Wide Web,
+# typically for the purpose of Web indexing.
+
+import requests
+from bs4 import BeautifulSoup
+
+def IrishCastles(max_pages):
+    page = 1
+    while page <= max_pages:
+        url = 'https://www.reddit.com/r/castles/#page=' + str(page)
+        source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text, 'html.parser')
+        for link in soup.findAll('a', {'class': 'title may-blank loggedin  srTagged imgScanned'}):
+            href = link.get('href')
+            print(href)
+        page += 1
+
+IrishCastles(1)
